07038a86524a52a0106fab5152741ab78853f0bd
Narsilien <rigaux.jonathan@gmail.com>
2025-07-28 10:09:35 +0200

    Simplify provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,21 +3,9 @@
 #region USE
 
 use App\Providers\AppServiceProvider;
-use Narsil\Providers\ComponentServiceProvider;
-use Narsil\Providers\FieldServiceProvider;
-use Narsil\Providers\FormRequestServiceProvider;
-use Narsil\Providers\FormServiceProvider;
-use Narsil\Providers\FortifyServiceProvider;
-use Narsil\Providers\TableServiceProvider;
 
 #endregion
 
 return [
     AppServiceProvider::class,
-    ComponentServiceProvider::class,
-    FieldServiceProvider::class,
-    FormServiceProvider::class,
-    FormRequestServiceProvider::class,
-    FortifyServiceProvider::class,
-    TableServiceProvider::class,
 ];
