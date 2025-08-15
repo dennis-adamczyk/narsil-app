f65645316b5296bafb3f07a0e28dde29692b8453
Narsilien <rigaux.jonathan@gmail.com>
2025-08-15 16:23:59 +0200

    Add graph QL

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,9 +3,11 @@
 #region USE
 
 use App\Providers\AppServiceProvider;
+use Nuwave\Lighthouse\LighthouseServiceProvider;
 
 #endregion
 
 return [
     AppServiceProvider::class,
+    LighthouseServiceProvider::class,
 ];
