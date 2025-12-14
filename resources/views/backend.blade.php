0b7226197dbb23d1c90b2a1d9eebaf70668286ea
Narsilien <rigaux.jonathan@gmail.com>
2025-12-14 12:39:27 +0100

    Format backend file

--- resources/views/backend.blade.php
+++ resources/views/backend.blade.php
@@ -2,23 +2,25 @@
 
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-<head>
-	<meta charset="utf-8" />
-	<meta
-		content="width=device-width, initial-scale=1.0, maximum-scale=5.0"
-		name="viewport" />
-	<link
-		href="/favicon.svg"
-		rel="icon" />
+	<head>
+		<meta charset="utf-8" />
+		<meta
+			content="width=device-width, initial-scale=1.0, maximum-scale=5.0"
+			name="viewport"
+		/>
+		<link
+			href="/favicon.svg"
+			rel="icon"
+		/>
 
-	@routes
-	@viteReactRefresh
-	@vite(['resources/css/backend.css', 'resources/js/backend.tsx'])
-	@inertiaHead
-</head>
+		@routes
+		@viteReactRefresh
+		@vite(['resources/css/backend.css', 'resources/js/backend.tsx'])
+		@inertiaHead
+	</head>
 
-<body class="antialiased">
-	@inertia
-</body>
+	<body class="antialiased">
+		@inertia
+	</body>
 
 </html>
