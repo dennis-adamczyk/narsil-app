1519855c4a3145346b21755a1b3f82df7d0b03d4
Jonathan Rigaux <jonathan.rigaux@rheinschafe.de>
2025-12-17 16:21:45 +0100

    Refactor page controller for flexibility

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -9,6 +9,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use Inertia\Inertia;
 use Inertia\Response;
+use Narsil\Contracts\Resources\FooterResource;
+use Narsil\Contracts\Resources\HeaderResource;
+use Narsil\Contracts\Resources\SitePageResource;
 use Narsil\Models\Sites\Site;
 use Narsil\Models\Sites\SitePage;
 use Narsil\Services\PageService;
@@ -30,16 +33,71 @@ class PageController extends Controller
      */
     public function __invoke(Request $request): Response
     {
-        $page = PageService::resolvePage($request);
+        $sitePage = PageService::resolvePage($request);
+
+        $header = $this->getHeader($sitePage);
+        $footer = $this->getFooter($sitePage);
+        $page = $this->getPage($sitePage);
+        $session = $this->getSession($sitePage);
 
         return Inertia::render('frontend/index', [
-            'footer' => $page->{SitePage::RELATION_SITE}->{Site::RELATION_FOOTER},
-            'header' => $page->{SitePage::RELATION_SITE}->{Site::RELATION_HEADER},
+            'footer' => $footer,
+            'header' => $header,
             'page' => $page,
-            'session' => [
-                'locale' => App::getLocale(),
-            ]
+            'session' => $session,
         ]);
+    }
+
+    #endregion
+
+    #region PRIVATE METHODS
+
+    /**
+     * @param SitePage $sitePage
+     *
+     * @return HeaderResource
+     */
+    private function getHeader(SitePage $sitePage): HeaderResource
+    {
+        return app(HeaderResource::class, [
+            'resource' => $sitePage->{SitePage::RELATION_SITE}->{Site::RELATION_HEADER},
+        ]);
+    }
+
+    /**
+     * @param SitePage $sitePage
+     *
+     * @return FooterResource
+     */
+    private function getFooter(SitePage $sitePage): FooterResource
+    {
+        return app(FooterResource::class, [
+            'resource' => $sitePage->{SitePage::RELATION_SITE}->{Site::RELATION_FOOTER},
+        ]);
+    }
+
+    /**
+     * @param SitePage $sitePage
+     *
+     * @return SitePageResource
+     */
+    private function getPage(SitePage $sitePage): SitePageResource
+    {
+        return app(SitePageResource::class, [
+            'resource' => $sitePage,
+        ]);
+    }
+
+    /**
+     * @param SitePage $sitePage
+     * 
+     * @return SitePageResource
+     */
+    private function getSession(SitePage $sitePage): array
+    {
+        return [
+            'locale' => App::getLocale(),
+        ];
     }
 
     #endregion
