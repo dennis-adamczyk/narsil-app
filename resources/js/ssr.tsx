bbafabf9c0e57a7aa3c995b1919a345da16f5aca
Dennis Adamczyk <ich@dennis-adamczyk.de>
2025-12-22 12:46:05 +0100

    feat: add server-side rendering (SSR) to the frontend

---
+++ resources/js/ssr.tsx
@@ -0,0 +1,33 @@
+import { createInertiaApp } from "@inertiajs/react";
+import createServer from "@inertiajs/react/server";
+import { type ComponentProps } from "react";
+import ReactDOMServer from "react-dom/server";
+import Layout from "./layouts/layout";
+
+createServer((page) =>
+  createInertiaApp({
+    page,
+    render: ReactDOMServer.renderToString,
+    resolve: (name) => {
+      const appPages = import.meta.glob("@/pages/**/*.tsx", {
+        eager: true,
+      });
+
+      const appKey = `/resources/js/pages/${name}.tsx`;
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const page: any = appPages[appKey];
+
+      if (!page) {
+        throw new Error(`Page not found: ${name}`);
+      }
+
+      page.default.layout =
+        page.default?.layout ||
+        ((page: ComponentProps<typeof Layout>["children"]) => <Layout children={page} />);
+
+      return page;
+    },
+    setup: ({ App, props }) => <App {...props} />,
+  }),
+);
