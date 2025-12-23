c29487190ece184b3deab4bde801fe8a98ede172
Dennis Adamczyk <ich@dennis-adamczyk.de>
2025-12-23 23:49:13 +0100

    feat: enable cluster mode for SSR to utilize all CPU cores

--- resources/js/ssr.tsx
+++ resources/js/ssr.tsx
@@ -4,34 +4,38 @@ import { type ComponentProps } from "react";
 import { renderToReadableStream, renderToString } from "react-dom/server";
 import Layout from "./layouts/layout";
 
-createServer((page) =>
-  createInertiaApp({
-    page,
-    render: (async (element: React.ReactNode) => {
-      const stream = await renderToReadableStream(element);
-      await stream.allReady;
-      return new Response(stream).text();
-    }) as unknown as typeof renderToString,
-    resolve: (name) => {
-      const appPages = import.meta.glob("@/pages/**/*.tsx", {
-        eager: true,
-      });
+createServer(
+  (page) =>
+    createInertiaApp({
+      page,
+      render: (async (element: React.ReactNode) => {
+        const stream = await renderToReadableStream(element);
+        await stream.allReady;
+        return new Response(stream).text();
+      }) as unknown as typeof renderToString,
+      resolve: (name) => {
+        const appPages = import.meta.glob("@/pages/**/*.tsx", {
+          eager: true,
+        });
 
-      const appKey = `/resources/js/pages/${name}.tsx`;
+        const appKey = `/resources/js/pages/${name}.tsx`;
 
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const page: any = appPages[appKey];
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const page: any = appPages[appKey];
 
-      if (!page) {
-        throw new Error(`Page not found: ${name}`);
-      }
+        if (!page) {
+          throw new Error(`Page not found: ${name}`);
+        }
 
-      page.default.layout =
-        page.default?.layout ||
-        ((page: ComponentProps<typeof Layout>["children"]) => <Layout children={page} />);
+        page.default.layout =
+          page.default?.layout ||
+          ((page: ComponentProps<typeof Layout>["children"]) => <Layout children={page} />);
 
-      return page;
-    },
-    setup: ({ App, props }) => <App {...props} />,
-  }),
+        return page;
+      },
+      setup: ({ App, props }) => <App {...props} />,
+    }),
+  {
+    cluster: true,
+  },
 );
